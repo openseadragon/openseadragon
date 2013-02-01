d8677c7f8cb38e4cf2e3dc5f86eebe4b729c1e38
Ian Gilman <ian@iangilman.com>
2013-02-01 15:29:14 -0800

    Checking in test

---
+++ test/test.js
@@ -0,0 +1,16 @@
+test( "hello test", function() {
+    ok( 1 == "1", "Passed!" );
+});
+
+(function() {
+
+  // $(document).ready(function() {
+    // OpenSeadragon({
+    //     id:            "example",
+    //     prefixUrl:     "/images/",
+    //     tileSources:   "/test/dzi/dog-and-cat-collars.xml", 
+    //     showNavigator:  true
+    // });
+  // });
+  
+})();
