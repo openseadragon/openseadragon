9401b5d393a79f464b1f32be550f0bbd13bf9c35
Ian Gilman <ian@iangilman.com>
2013-03-07 10:44:08 -0800

    First version of real tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,22 @@
-test( "hello test", function() {
-    ok( 1 == "1", "Passed!" );
-});
-
 (function() {
 
-  // $(document).ready(function() {
-    // OpenSeadragon({
-    //     id:            "example",
-    //     prefixUrl:     "/images/",
-    //     tileSources:   "/test/dzi/dog-and-cat-collars.xml", 
-    //     showNavigator:  true
-    // });
-  // });
+    asyncTest('OpenSeadragon', function() {
+        $(document).ready(function() {
+            var viewer = OpenSeadragon({
+                id:            'example',
+                prefixUrl:     '/build/openseadragon/images/',
+                tileSources:   '/test/data/testpattern.dzi', 
+                showNavigator:  true
+            });
+
+            ok(viewer, 'Viewer exists');
+            viewer.addHandler('open', function(eventSender, eventData) {
+                ok(true, 'Open event was sent');
+                ok(eventSender === viewer, 'Sender of open event was viewer');
+                ok(eventData, 'Handler also received event data');
+                start();
+            });
+        });
+    });  
   
 })();
