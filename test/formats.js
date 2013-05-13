9001fe9a5e86c77d002432059d0c4bc0da88a473
Ian Gilman <ian@iangilman.com>
2013-05-13 10:50:43 -0700

    Generalized format test and added DZI XML

--- test/formats.js
+++ test/formats.js
@@ -1,18 +1,21 @@
 (function() {
 
-    module('DZI JSONp');
+    // This module tests whether our various file formats can be opened.
+    // TODO: Add more file formats (with corresponding test data).
+
+    module('Formats');
 
     var viewer = null;
 
     // ----------
-    asyncTest('Open', function() {
+    var testOpen = function(name) {
         $(document).ready(function() {
             var timeWatcher = Util.timeWatcher();
 
             viewer = OpenSeadragon({
                 id:            'example',
                 prefixUrl:     '/build/openseadragon/images/',
-                tileSources:   '/test/data/testpattern.js'
+                tileSources:   '/test/data/' + name
             });
 
             ok(viewer, 'Viewer exists');
@@ -27,24 +30,34 @@
             var tileDrawnHandler = function(eventSender, eventData) {
                 viewer.removeHandler('tile-drawn', tileDrawnHandler);
                 ok(true, 'A tile has been drawn');
+                viewer.addHandler('close', closeHandler);
+                viewer.close();
+            };
+
+            var closeHandler = function() {
+                viewer.removeHandler('close', closeHandler);
+                $('#example').empty();
+                ok(true, 'Close event was sent');
                 timeWatcher.done();
             };
 
             viewer.addHandler('open', openHandler);
         });
+    };
+
+    // ----------
+    asyncTest('DZI', function() {
+        testOpen('testpattern.dzi');
     });
 
     // ----------
-    asyncTest('Close', function() {
-        var closeHandler = function() {
-            viewer.removeHandler('close', closeHandler);
-            $('#example').empty();
-            ok(true, 'Close event was sent');
-            start();
-        };
-
-        viewer.addHandler('close', closeHandler);
-        viewer.close();
+    asyncTest('DZI JSONp', function() {
+        testOpen('testpattern.js');
     });
-  
+
+    // ----------
+    asyncTest('DZI XML', function() {
+        testOpen('testpattern.xml');
+    });
+
 })();
