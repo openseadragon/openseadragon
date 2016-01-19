41ee1e27fd6c6c220d6532c3b05318cad7763869
Mei-Hui Su <mei@Mei-Huis-MacBook-Air.local>
2016-01-19 14:29:15 -0800

    put back the 'options.'

--- src/drawer.js
+++ src/drawer.js
@@ -370,7 +370,7 @@ $.Drawer.prototype = /** @lends OpenSeadragon.Drawer.prototype */{
      * @param {Float} [scale=1] The scale at which tiles were drawn on the sketch. Default is 1.
      *   Use scale to draw at a lower scale and then enlarge onto the main canvas.
      * @param {OpenSeadragon.Point} [translate] A translation vector that was used to draw the tiles
-     * @param {String} [compositeOperation] - How the image is composited onto other images; see compositeOperation in {@link OpenSeadragon.Options} for possible values.
+     * @param {String} [options.compositeOperation] - How the image is composited onto other images; see compositeOperation in {@link OpenSeadragon.Options} for possible values.
      * @returns {undefined}
      */
     blendSketch: function(opacity, scale, translate, compositeOperation) {
