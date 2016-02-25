7d4eff0bb881a3fa5110c5b3ef253a69464fc0fd
Ralph Krimmel <rkrimme1@gwdg.de>
2016-02-25 10:16:08 +0100

    Minor style fixes

--- src/zoomifytilesource.js
+++ src/zoomifytilesource.js
@@ -1,4 +1,4 @@
-(function( $ ){
+(function($) {
 
   /**
    * @class ZoomifyTileSource
@@ -11,46 +11,60 @@
    * @param {Number} tileSize
    * @param {String} tilesUrl
    */
-  $.ZoomifyTileSource = function( options ) {
+  $.ZoomifyTileSource = function(options) {
 
-  	options.tileSize = 256;
-		
-		var currentImageSize = {x:options.width,y:options.height};
-		options.imageSizes = [{x:options.width,y:options.height}];
-  	options.gridSize = [this._getGridSize(options.width,options.height,options.tileSize)];
-		
-  	while(parseInt(currentImageSize.x) > options.tileSize || parseInt(currentImageSize.y) > options.tileSize) {
-				currentImageSize.x = Math.floor(currentImageSize.x/2);
-				currentImageSize.y = Math.floor(currentImageSize.y/2);
-				options.imageSizes.push({x:currentImageSize.x,y:currentImageSize.y});
-				options.gridSize.push(this._getGridSize(currentImageSize.x,currentImageSize.y,options.tileSize));
-			}
-		options.imageSizes.reverse();
-		options.gridSize.reverse();
-		options.minLevel = 0;
-		options.maxLevel = options.gridSize.length-1;
-		
-  	
-  	OpenSeadragon.TileSource.apply( this, [ options ] );
+    options.tileSize = 256;
+
+    var currentImageSize = {
+      x: options.width,
+      y: options.height
+    };
+    options.imageSizes = [{
+      x: options.width,
+      y: options.height
+    }];
+    options.gridSize = [this._getGridSize(options.width, options.height, options.tileSize)];
+
+    while (parseInt(currentImageSize.x, 10) > options.tileSize || parseInt(currentImageSize.y, 10) > options.tileSize) {
+      currentImageSize.x = Math.floor(currentImageSize.x / 2);
+      currentImageSize.y = Math.floor(currentImageSize.y / 2);
+      options.imageSizes.push({
+        x: currentImageSize.x,
+        y: currentImageSize.y
+      });
+      options.gridSize.push(this._getGridSize(currentImageSize.x, currentImageSize.y, options.tileSize));
+    }
+    options.imageSizes.reverse();
+    options.gridSize.reverse();
+    options.minLevel = 0;
+    options.maxLevel = options.gridSize.length - 1;
+
+
+    OpenSeadragon.TileSource.apply(this, [options]);
 
   };
 
-  $.extend( $.ZoomifyTileSource.prototype, $.TileSource.prototype, /** @lends OpenSeadragon.ZoomifyTileSource.prototype */{
-		
-		_getGridSize: function(width,height,tileSize) {
-			return {x: Math.ceil(width/tileSize),y:Math.ceil(height/tileSize)};
-		},
-		
-		_calculateAbsoluteTileNumber: function (level, x, y) {
-			var num = 0;
-			for (var z = 0; z < level; z++) {
-				var size = this.gridSize[z];
-				num += size.x * size.y;
-			}
-			var size = this.gridSize[level];
-			num += size.x*y+ x
-			return num;
-		},
+  $.extend($.ZoomifyTileSource.prototype, $.TileSource.prototype, /** @lends OpenSeadragon.ZoomifyTileSource.prototype */ {
+
+    //private
+    _getGridSize: function(width, height, tileSize) {
+      return {
+        x: Math.ceil(width / tileSize),
+        y: Math.ceil(height / tileSize)
+      };
+    },
+
+    //private
+    _calculateAbsoluteTileNumber: function(level, x, y) {
+      var num = 0;
+      for (var z = 0; z < level; z++) {
+        var size = this.gridSize[z];
+        num += size.x * size.y;
+      }
+      var size = this.gridSize[level];
+      num += size.x * y + x
+      return num;
+    },
 
     /**
      * Determine if the data and/or url imply the image service is supported by
@@ -59,9 +73,8 @@
      * @param {Object|Array} data
      * @param {String} optional - url
      */
-    supports: function( data, url ){
-      //console.log('ZoomifyTileSource supports ' + data.type); 
-      return ( data.type && "zoomifytileservice" == data.type );
+    supports: function(data, url) {
+      return (data.type && "zoomifytileservice" == data.type);
     },
 
     /**
@@ -72,8 +85,7 @@
      * @return {Object} options - A dictionary of keyword arguments sufficient
      *      to configure this tile sources constructor.
      */
-    configure: function( data, url ){
-      //console.log('ZoomifyTileSource configure ' + data.tilesUrl);
+    configure: function(data, url) {
       return data;
     },
 
@@ -84,14 +96,14 @@
      * @param {Number} x
      * @param {Number} y
      */
-    getTileUrl: function( level, x, y ){
-			//console.log(level);
-			var result = 0;
-			var num = this._calculateAbsoluteTileNumber(level,x,y);
-			result = Math.floor(num/256);
-			return  this.tilesUrl + 'TileGroup' + result + '/' + level + '-' + x + '-' + y + '.jpg';
-      
-    },          
+    getTileUrl: function(level, x, y) {
+      //console.log(level);
+      var result = 0;
+      var num = this._calculateAbsoluteTileNumber(level, x, y);
+      result = Math.floor(num / 256);
+      return this.tilesUrl + 'TileGroup' + result + '/' + level + '-' + x + '-' + y + '.jpg';
+
+    }
   });
-	
-}( OpenSeadragon ));
+
+}(OpenSeadragon));
