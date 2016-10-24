b992d545721c9343c7db70285c93bab6ba2cac09
Antoine Vandecreme <ant.vand@gmail.com>
2016-10-24 21:34:29 +0200

    Fix jshint error

--- src/tiledimage.js
+++ src/tiledimage.js
@@ -1545,7 +1545,7 @@ function drawTiles( tiledImage, lastDrawn ) {
     var imageZoom = tiledImage.viewportToImageZoom(zoom);
     // TODO: support tile edge smoothing with tiled image rotation.
     if (imageZoom > tiledImage.smoothTileEdgesMinZoom && !tiledImage.iOSDevice &&
-        tiledImage.getRotation() == 0) {
+        tiledImage.getRotation() === 0) {
         // When zoomed in a lot (>100%) the tile edges are visible.
         // So we have to composite them at ~100% and scale them up together.
         // Note: Disabled on iOS devices per default as it causes a native crash
