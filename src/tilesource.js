6370a6bafaedfe1548c79254c9ddb77829e32209
Antoine Vandecreme <ant.vand@gmail.com>
2016-11-02 22:35:23 +0100

    Fix tests

--- src/tilesource.js
+++ src/tilesource.js
@@ -347,7 +347,7 @@ $.TileSource.prototype = {
     getTileAtPoint: function(level, point) {
         var validPoint = point.x >= 0 && point.x <= 1 &&
             point.y >= 0 && point.y <= 1 / this.aspectRatio;
-        $.console.assert(validPoint, "[TileSource.getTileAtPoint] called with invalid point " + point.toString());
+        $.console.assert(validPoint, "[TileSource.getTileAtPoint] must be called with a valid point.");
 
         var widthScaled = this.dimensions.x * this.getLevelScale(level);
         var pixelX = point.x * widthScaled;
