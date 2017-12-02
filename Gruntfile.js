343e990738c71aeb52bc72b9649368e53ea736ca
nein09 <nein09@nein09.com>
2017-12-01 16:21:08 -0800

    Replace grunt-qunit-istanbul with grunt-contrib-qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-compress");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-qunit-istanbul");
+    grunt.loadNpmTasks("grunt-contrib-qunit");
     grunt.loadNpmTasks("grunt-contrib-connect");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-clean");
