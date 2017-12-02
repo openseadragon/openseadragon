c29715cbe8c70384360a7a915dcaaae364324ebf
nein09 <nein09@nein09.com>
2017-12-01 16:52:34 -0800

    Update QUnit syntax in strings tests

--- test/modules/strings.js
+++ test/modules/strings.js
@@ -1,44 +1,44 @@
-/* global module, asyncTest, $, ok, equal, notEqual, start, test, Util, testLog */
+/* global QUnit, testLog */
 
 (function() {
 
-    module("strings", {
-        setup: function () {
+    QUnit.module("strings", {
+        beforeEach: function () {
             testLog.reset();
         }
     });
 
-    test("getSubString", function() {
-        equal(OpenSeadragon.getString("Errors.Dzi"),
+    QUnit.test("getSubString", function(assert) {
+        assert.equal(OpenSeadragon.getString("Errors.Dzi"),
             "Hmm, this doesn't appear to be a valid Deep Zoom Image.",
             "Read sub-string");
     });
 
-    test("getStringWithPlaceholders", function() {
-        equal(OpenSeadragon.getString("Errors.OpenFailed", "foo", "bar"),
+    QUnit.test("getStringWithPlaceholders", function(assert) {
+        assert.equal(OpenSeadragon.getString("Errors.OpenFailed", "foo", "bar"),
               "Unable to open foo: bar",
               "String placeholder replacement");
     });
 
-    test("getInvalidString", function() {
-        equal(OpenSeadragon.getString("Greeting"), "", "Handled unset string key");
-        ok(testLog.log.contains('["Untranslated source string:","Greeting"]'),
+    QUnit.test("getInvalidString", function(assert) {
+        assert.equal(OpenSeadragon.getString("Greeting"), "", "Handled unset string key");
+        assert.ok(testLog.log.contains('["Untranslated source string:","Greeting"]'),
                                   'Invalid string keys are logged');
 
-        equal(OpenSeadragon.getString("Errors"), "", "Handled requesting parent key");
-        ok(testLog.log.contains('["Untranslated source string:","Errors"]'),
+        assert.equal(OpenSeadragon.getString("Errors"), "", "Handled requesting parent key");
+        assert.ok(testLog.log.contains('["Untranslated source string:","Errors"]'),
                                   'Invalid string parent keys are logged');
     });
 
-    test("setString", function() {
+    QUnit.test("setString", function(assert) {
         OpenSeadragon.setString("Greeting", "Hello world");
-        equal(OpenSeadragon.getString("Greeting"), "Hello world",
+        assert.equal(OpenSeadragon.getString("Greeting"), "Hello world",
             "Set a string");
     });
 
-    test("setSubString", function() {
+    QUnit.test("setSubString", function(assert) {
        OpenSeadragon.setString("CustomGreeting.Hello", "Hello world");
-       equal(OpenSeadragon.getString("CustomGreeting.Hello"), "Hello world",
+       assert.equal(OpenSeadragon.getString("CustomGreeting.Hello"), "Hello world",
            "Set a sub-string");
     });
 
