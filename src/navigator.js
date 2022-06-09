3ca17c1508b980e5f371575df9195a76c006cce5
Joachim de fourestier <jdefourestier@fibics.com>
2022-06-08 20:15:21 -0400

    missing period

--- src/navigator.js
+++ src/navigator.js
@@ -46,7 +46,7 @@
  * @extends OpenSeadragon.Viewer
  * @extends OpenSeadragon.EventSource
  * @param {Object} options - Navigator options
- * @param {Element} [options.element] - An element to use for the navigator
+ * @param {Element} [options.element] - An element to use for the navigator.
  * @param {String} [options.id] - Id of the element to use for the navigator. However, this is ignored if {@link options.element} is provided.
  */
 $.Navigator = function( options ){
