77bc130636ab01246739f1d92f93b710eed8002c
Dragos Daian <daian.dragos@yahoo.com>
2023-01-23 19:49:43 +0100

    Add tileRetryMax documentation.

--- src/openseadragon.js
+++ src/openseadragon.js
@@ -495,6 +495,9 @@
   * @property {Number} [timeout=30000]
   *     The max number of milliseconds that an image job may take to complete.
   *
+  * @property {Number} [tileRetryMax=0]
+  *     The max number of retries when a tile download fails. By default it's 0, so retries are disabled.
+  *
   * @property {Boolean} [useCanvas=true]
   *     Set to false to not use an HTML canvas element for image rendering even if canvas is supported.
   *
