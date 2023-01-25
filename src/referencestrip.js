8550b4fea51429e2060c47696c1fe31041ac5aad
Damian Murawski <damon6363@wp.pl>
2023-01-25 23:15:12 +0100

    fix problem with click precision on ReferenceStrip
    temporary fix for #1992 . Just adding 4px which works in all use cases.

--- src/referencestrip.js
+++ src/referencestrip.js
@@ -300,7 +300,8 @@ function onStripClick( event ) {
         var page;
 
         if ( 'horizontal' === this.scroll ) {
-            page = Math.floor(event.position.x / this.panelWidth);
+            // +4px fix to solve problem with precision on thumbnail selection if there is a lot of them 
+            page = Math.floor(event.position.x / (this.panelWidth + 4));
         } else {
             page = Math.floor(event.position.y / this.panelHeight);
         }
