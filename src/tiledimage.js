0bdd807d9d027fc418aef59b768b3f1c56f27ed9
Peter <jetic@gmx.net>
2023-08-08 11:05:33 +0200

    Cleaned code according to comments in PR

--- src/tiledimage.js
+++ src/tiledimage.js
@@ -1437,7 +1437,6 @@ $.extend($.TiledImage.prototype, $.EventSource.prototype, /** @lends OpenSeadrag
             }
         }
 
-        this._sortTiles(best);
         return best;
     },
 
@@ -1931,8 +1930,8 @@ $.extend($.TiledImage.prototype, $.EventSource.prototype, /** @lends OpenSeadrag
             return [tile];
         }
         previousBest.push(tile);
+        this._sortTiles(previousBest);
         if (previousBest.length > maxNTiles) {
-            this._sortTiles(previousBest);
             previousBest.pop();
         }
         return previousBest;
