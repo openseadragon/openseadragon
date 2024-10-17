06ac68d00ef498ae82b7d274356d4fe009deef1a
Aiosa <469130@mail.muni.cz>
2024-10-17 12:17:49 +0200

    Html drawer add canvas to supported formats

--- src/htmldrawer.js
+++ src/htmldrawer.js
@@ -129,7 +129,7 @@ class HTMLDrawer extends OpenSeadragon.DrawerBase{
     }
 
     getSupportedDataFormats() {
-        return [HTMLDrawer.imageCacheType];
+        return [HTMLDrawer.imageCacheType, HTMLDrawer.canvasCacheType];
     }
 
     /**
