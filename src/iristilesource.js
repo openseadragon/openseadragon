a88442221c6abeaca3d5db682a0bd1be39b8565e
Navin Kathawa <nkathawa@umich.edu>
2025-07-14 13:39:21 -0400

    fully zooming without using maxZoomLevel, fix tests

--- src/iristilesource.js
+++ src/iristilesource.js
@@ -87,27 +87,33 @@
       this._tileWidth = 256;
       this._tileHeight = 256;
 
-      this.width = data.extent.width;
-      this.height = data.extent.height;
+      this.tileSize = this._tileWidth;
+      this.tileOverlap = 0;
 
-      if (this.width <= 0 || this.height <= 0) {
-        const msg = "Invalid dimensions received from metadata.";
-        $.console.error(msg);
-        this.raiseEvent('open-failed', { message: msg });
-        return;
-      }
+      const layers = data.extent.layers;
+
+      const maxLayer = layers.length - 1;
+      const maxScale = layers[maxLayer].scale;
+      this.width = data.extent.width * maxScale;
+      this.height = data.extent.height * maxScale;
 
       this.dimensions = new $.Point(this.width, this.height);
       this.aspectRatio = this.width / this.height;
-
-      this.levelSizes = data.extent.layers.map(level => ({
+      this.levelSizes = layers.map(level => ({
         width: level.x_tiles * this._tileWidth,
         height: level.y_tiles * this._tileHeight,
-        xTiles: level.x_tiles
+        xTiles: level.x_tiles,
+        yTiles: level.y_tiles
       }));
 
-      this.levelScales = data.extent.layers.map(level => level.scale);
-      this.maxLevel = data.extent.layers.length - 1;
+      const fullResWidth = this.levelSizes[this.levelSizes.length - 1].width;
+
+      this.levelScales = this.levelSizes.map(level =>
+        level.width / fullResWidth
+      );
+
+      this.minLevel = 0;
+      this.maxLevel = this.levelSizes.length - 1;
     },
 
     defaultFetchMetadata: function(url, context) {
@@ -137,18 +143,15 @@
     },
 
     getNumTiles: function(level) {
-      var levelSize = this.levelSizes[level];
-      var x = Math.ceil(levelSize.width / this._tileWidth);
-      var y = Math.ceil(levelSize.height / this._tileHeight);
-
-      return new $.Point(x, y);
+      if (level < this.minLevel || level > this.maxLevel || !this.levelSizes[level]) {
+        return new $.Point(0, 0);
+      }
+      return new $.Point(this.levelSizes[level].xTiles, this.levelSizes[level].yTiles);
     },
 
     getTileUrl: function(level, x, y) {
       const pos = y * this.levelSizes[level].xTiles + x;
-      const url = `${this.serverUrl}/slides/${this.slideId}/layers/${level}/tiles/${pos}`;
-
-      return url;
+      return `${this.serverUrl}/slides/${this.slideId}/layers/${level}/tiles/${pos}`;
     },
 
     getLevelScale: function(level) {
--- test/modules/iris.js
+++ test/modules/iris.js
@@ -1,20 +1,21 @@
 (function() {
     QUnit.module('Iris');
 
+    const mockMetadata = {
+        extent: {
+            width: 1983,
+            height: 1381,
+            layers: [
+                { x_tiles: 8, y_tiles: 6, scale: 1 },
+                { x_tiles: 31, y_tiles: 22, scale: 4.0005 },
+                { x_tiles: 124, y_tiles: 87, scale: 16.0035 },
+                { x_tiles: 496, y_tiles: 346, scale: 64.0141 }
+            ]
+        }
+    };
+
     const mockFetchMetadata = function(url, context) {
-        const metadata = {
-            extent: {
-                width: 1983,
-                height: 1381,
-                layers: [
-                    { x_tiles: 8, y_tiles: 6, scale: 1 },
-                    { x_tiles: 31, y_tiles: 22, scale: 4.0005 },
-                    { x_tiles: 124, y_tiles: 87, scale: 16.0035 },
-                    { x_tiles: 496, y_tiles: 346, scale: 64.0141 }
-                ]
-            }
-        };
-        context.parseMetadata(metadata);
+        context.parseMetadata(mockMetadata);
         context.ready = true;
         context.raiseEvent('ready', { tileSource: context });
     };
@@ -27,7 +28,7 @@
         });
 
         assert.ok(test.ready, "IrisTileSource should be ready after mock metadata");
-        assert.equal(test.width, 1983, "Width should be correctly parsed from mock metadata");
+        assert.equal(test.width, mockMetadata.extent.width * mockMetadata.extent.layers[mockMetadata.extent.layers.length - 1].scale, "Width should be correctly parsed from mock metadata");
 
         const expectedUrl = "http://localhost/slides/12345/metadata";
         assert.equal(test.getMetadataUrl(), expectedUrl, "Metadata URL should match expected format");
@@ -40,35 +41,31 @@
             fetchMetadata: () => {}
         });
 
-        const metadata = {
-            extent: {
-                width: 1983,
-                height: 1381,
-                layers: [
-                    { x_tiles: 8, y_tiles: 6, scale: 1 },
-                    { x_tiles: 31, y_tiles: 22, scale: 4.0005 },
-                    { x_tiles: 124, y_tiles: 87, scale: 16.0035 },
-                    { x_tiles: 496, y_tiles: 346, scale: 64.0141 }
-                ]
-            }
-        };
-
-        test.parseMetadata(metadata);
+        test.parseMetadata(mockMetadata);
 
         // Check dimensions
         assert.ok(test.width, "Width exists");
-        assert.equal(test.width, 1983, "Parsing width correctly");
-        assert.equal(test.height, 1381, "Parsing height correctly");
+        assert.equal(test.width, mockMetadata.extent.width * mockMetadata.extent.layers[mockMetadata.extent.layers.length - 1].scale, "Parsing width correctly");
+        assert.equal(test.height, mockMetadata.extent.height * mockMetadata.extent.layers[mockMetadata.extent.layers.length - 1].scale, "Parsing height correctly");
 
         // Check level sizes and scales
         assert.ok(test.levelSizes, "Level sizes exist");
         assert.ok(Array.isArray(test.levelSizes), "Level sizes is an array");
-        assert.equal(test.levelSizes.length, 4, "Number of levels is correct");
-        assert.equal(test.maxLevel, 3, "Max level is correctly set");
+        assert.equal(test.levelSizes.length, mockMetadata.extent.layers.length, "Number of levels is correct");
+        assert.equal(test.maxLevel, mockMetadata.extent.layers.length - 1, "Max level is correctly set");
 
         assert.ok(test.levelScales, "Level scales exist");
         assert.equal(test.levelScales.length, 4, "Number of scales matches number of levels");
-        assert.deepEqual(test.levelScales, [1, 4.0005, 16.0035, 64.0141], "Parsing scales correctly");
+        assert.deepEqual(
+            test.levelScales,
+            [
+                mockMetadata.extent.layers[0].x_tiles / (mockMetadata.extent.layers[3].x_tiles),
+                mockMetadata.extent.layers[1].x_tiles / (mockMetadata.extent.layers[3].x_tiles),
+                mockMetadata.extent.layers[2].x_tiles / (mockMetadata.extent.layers[3].x_tiles),
+                mockMetadata.extent.layers[3].x_tiles / (mockMetadata.extent.layers[3].x_tiles)
+            ],
+            "Parsing scales correctly"
+        );
     });
 
     QUnit.test('IrisTileSource getTileUrl', function(assert) {
