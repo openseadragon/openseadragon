2c254d0d87fe68261d8ccfd8ae8e0289e9bd8f1f
Navin Kathawa <nkathawa@umich.edu>
2025-07-18 14:27:41 -0400

    rebuild

--- test/modules/iris.js
+++ test/modules/iris.js
@@ -48,7 +48,7 @@
 
         test.parseMetadata(mockMetadata);
 
-        // Check dimensions
+        // Check the dimensions
         const expectedWidth = parseInt(Math.ceil(
             mockMetadata.extent.width * mockMetadata.extent.layers[mockMetadata.extent.layers.length - 1].scale
         ), 10);
