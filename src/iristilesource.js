3f20ee37e851bb1a34e1d6e0dbc0963028a96753
Navin Kathawa <nkathawa@umich.edu>
2025-07-25 13:33:53 -0400

    doc comments

--- src/iristilesource.js
+++ src/iristilesource.js
@@ -68,14 +68,30 @@
   };
 
   $.extend($.IrisTileSource.prototype, $.TileSource.prototype, {
+    /**
+     * Return URL string for image metadata
+     * @function
+     * @returns {String} url - The Iris metadata URL
+     */
     getMetadataUrl: function() {
       return this.serverUrl + '/slides/' + this.slideId + '/metadata';
     },
 
+    /**
+     * Determine if the data implies the image service is supported by this tile source.
+     * @function
+     * @param {Object} data - The raw metadata object to check
+     * @returns {Boolean} - True if supported, false otherwise
+     */
     supports: function(data) {
       return (data && data.type === "iris" && data.serverUrl && data.slideId);
     },
 
+    /**
+     * Parse Iris protocol metadata response
+     * @function
+     * @param {Object} data - Raw metadata from an Iris server
+     */
     parseMetadata: function(data) {
       this._tileWidth = 256;
       this._tileHeight = 256;
@@ -106,6 +122,12 @@
       this.maxLevel = Math.ceil(this.levelSizes.length - 1);
     },
 
+    /**
+     * Default method to retrieve image metadata from an Iris-compatible server
+     * @function
+     * @param {String} url - The metadata URL
+     * @param {Object} context - The context (the tile source instance)
+     */
     defaultFetchMetadata: function(url, context) {
       $.makeAjaxRequest({
         url: url,
@@ -132,6 +154,12 @@
       });
     },
 
+    /**
+     * Get the number of tiles at a given level
+     * @function
+     * @param {Number} level - The image depth level
+     * @returns {OpenSeadragon.Point} - Number of tiles in x and y directions
+     */
     getNumTiles: function(level) {
       if (level < this.minLevel || level > this.maxLevel || !this.levelSizes[level]) {
         return new $.Point(0, 0);
@@ -142,15 +170,35 @@
       );
     },
 
+    /**
+     * Determine the URL which will return an image for the region specified by the given x, y, and level components.
+     * @function
+     * @param {Number} level - The zoom level
+     * @param {Number} x - The x tile index
+     * @param {Number} y - The y tile index
+     * @returns {String} - The tile URL
+     */
     getTileUrl: function(level, x, y) {
       const pos = y * this.levelSizes[level].xTiles + x;
       return `${this.serverUrl}/slides/${this.slideId}/layers/${level}/tiles/${pos}`;
     },
 
+    /**
+     * Get the scale for a given level
+     * @function
+     * @param {Number} level - The image depth level
+     * @returns {Number} - The scale for the level
+     */
     getLevelScale: function(level) {
       return this.levelScales[level];
     },
 
+    /**
+     * Retrieve and immediately return the options object
+     * @function
+     * @param {Object} options - Options object
+     * @returns {Object} - The options object
+     */
     configure: function (options) {
       return options;
     }
