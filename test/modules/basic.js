a7af08b628d91682c378e8f63fcfa165a77cd117
lucifer2661 <aditya.hackerface@gmail.com>
2026-08-12 01:43:54 +0530

    test: add positiveModulo unit tests to basic.js

--- test/modules/basic.js
+++ test/modules/basic.js
@@ -463,4 +463,11 @@
             assert.ok(OpenSeadragon.version.revision >= 0, "revision should be a positive number");
         });
     }
+    // Ensures positiveModulo returns a positive result for negative inputs
+    QUnit.test('positiveModulo', function(assert) {
+        assert.equal(OpenSeadragon.positiveModulo(-1, 5), 4, 'positiveModulo(-1, 5) should be 4');
+        assert.equal(OpenSeadragon.positiveModulo(-8, 7), 6, 'positiveModulo(-8, 7) should be 6');
+    });
+
+
 })();
